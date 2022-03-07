bc7e5234fc56cfad3a008157928ffc1258101de5
shivamkrishna <74373566+shivamkrishna@users.noreply.github.com>
2022-03-07 23:53:33 +0530

    Tree: Check is valid BST

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -5,6 +5,8 @@
 //  Created by shivam krishna on 05/03/22.
 //
 #include <iostream>
+#include <math.h>
+
 using namespace std;
 struct node{
     int data;
@@ -24,6 +26,20 @@ void display(node * node){
     display(node->left);
     display(node->right);
 }
+bool solve(node* root,long long mini,long long maxi){
+       if(!root) return true;
+       else if(root->data <= mini or root->data >= maxi) return false;
+       
+       bool ls = solve(root->left,mini,root->data);
+       bool rs = solve(root->right,root->data,maxi);
+       bool res = ls and rs;
+       return res;
+   }
+bool isValidBST(node* root){
+       long long lowRange = pow(-2,31)-1;
+       long long highRange = pow(2,31);
+       return solve(root,lowRange,highRange);
+}
 int main() {
     node* root = newNode(1);
     root->right = newNode(3);
@@ -31,6 +47,7 @@ int main() {
     root->left->left = newNode(4);
     root->left->right = newNode(5);
     display(root);
+    cout<<isValidBST(root)<<endl;
     // 1 2 4 5 3
     return 0;
 }
